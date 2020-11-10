8a457d91ae865ebec8d8c96c45bb38d7314a2dd6
Ibrahim Muhammed Abubaker Muhammed <ibrahimAlgadi.email@gmail.com>
2020-11-10 21:35:22 +0200

    - Creating Database Tables

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "mysql",
+        "host": `${process.env.DATABASE_HOST || '127.0.0.1'}`,
+        "port": `${process.env.DATABASE_PORT || 3306}`,
+        "database": `${process.env.DATABASE_NAME || 'sudanna_db'}`,
+        "username": `${process.env.DATABASE_USERNAME || 'root'}`,
+        "password": `${process.env.DATABASE_PASSWORD || ''}`
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+      "options": {
+        "ssl": false
+      }
+    }
+  }
 });
